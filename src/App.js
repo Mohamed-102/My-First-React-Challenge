ef1bdc146571ab55807ee01a8fabe9b13c7f00cc
Mohamed EL Qadi <elqadimohmed864@gmail.com>
2025-10-26 21:47:05 +0100

    Change code for using List Rendering

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import SideMenu from './SideMenu/SideMenu';
 
 const showCategories = true;
 function App() {
+  const posts = [
+    {id:1 ,title: "20 Tarmeez Academy", description: "أكاديمية مخصصة لتعليم البرمجة بمختلف لغاتها و تقنياتها"},
+    {id:2 ,title: "Hello World", description: "This is the hello world article"},
+    {id:3 ,title: "Post 3", description: "This is the body of post 3"},
+    {id:4 ,title: "Post 4", description: "This is the body of post 4"},
+    {id:5 ,title: "", description: ""},
+  ];
+
+  const postList = posts.map((post) => {
+    return(
+      <Post key={post.id} title={post.title} description={post.description} />
+    );
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,11 +28,7 @@ function App() {
       <div style={{display: "flex", justifyContent: "center"}}>
         <div style={{display: "flex",width: "60%"}}>
           <div style={{width: "70%"}}>
-            <Post title="20 Tarmeez Academy" description="أكاديمية مخصصة لتعليم البرمجة بمختلف لغاتها و تقنياتها" />
-            <Post title="Hello World" description="This is the hello world article" />
-            <Post title="Post 3" description="This is the body of post 3" />
-            <Post title="Post 4" description="This is the body of post 4" />
-            <Post />
+            {postList}
           </div>
           <div style={{width: "30%"}}>
             <AppSideMenu />
--- src/SideMenu/SideMenu.js
+++ src/SideMenu/SideMenu.js
@@ -1,13 +1,18 @@
 import Btn from "./Button/button";
+import "./Button/button.css";
 
 export default function SideMenu(){
-    return(
-        <div style={{margin: "25px", border: "solid teal 5px", width: "100%", textAlign: "center"}}>
-            <Btn title="New">
-                <p>💫💫💫</p>
-            </Btn>
+    const categories = [
+        {
+            id:1 ,
+            title: "New",
+            children: (<p>💫💫💫</p>),
+        },
 
-            <Btn title="The most readable">
+        {
+            id:2 ,
+            title: "The most readable",
+            children: (
                 <div>
                     <img src="https://cdn.pixabay.com/photo/2016/05/15/15/12/hepatica-1393813_640.jpg" alt="flowerImage"
                         style={{
@@ -15,9 +20,13 @@ export default function SideMenu(){
                         }}
                     />
                 </div>
-            </Btn>
-            
-            <Btn title="Special Articles">
+            ),
+        },
+
+        {
+            id:3 ,
+            title: "Special Articles", 
+            children: (
                 <div>
                     <p style={{fontSize: "20px"}}>🌟🌟🌟</p>
                     <img src="https://images.unsplash.com/photo-1538998073820-4dfa76300194?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZSUyMGZsb3dlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500"
@@ -27,8 +36,27 @@ export default function SideMenu(){
                         }}
                     />
                 </div>
+            ),
+        },
+
+        {
+            id:4 ,
+            title: "", 
+            children: (<div></div>),
+        },
+    ];
+
+    const categoriesList = categories.map((cat) => {
+        return(
+            <Btn key={cat.id} title={cat.title} >
+                {cat.children}    
             </Btn>
-            <Btn title=""/>
-        </div>
+        );
+    })
+
+    return(
+        <div style={{margin: "25px", border: "solid teal 5px", width: "100%", textAlign: "center"}}>
+            {categoriesList}
+        </div> 
     );
 }
